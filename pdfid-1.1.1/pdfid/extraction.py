2922302afd486a858ba0a0d96144ca400dd47db7
ippo <lorenzoippolito.99@gmail.com>
2022-10-23 18:56:51 +0200

    extract from pdfs

--- pdfid-1.1.1/pdfid/extraction.py
+++ pdfid-1.1.1/pdfid/extraction.py
@@ -5,7 +5,7 @@ import collections
 
 def pdf_analysis(filename):
 	xml = PDFiD(filename)
-	doc = PDFiD2JSON(xml, False)
+	doc = PDFiD2JSON(xml, True)
 
 	y = json.loads(doc)
 	js = json.loads(json.dumps(y[0]))
@@ -15,23 +15,35 @@ def pdf_analysis(filename):
 		name = w["name"]
 		count = w["count"]
 		dic.update({name:count})
-	
 	return dic
 
-if __name__ == '__main__':
-
-	#directory = "../../../pro/"
-	directory = "../../pdf_data/clean_pdf_9000_files/"
+def dir_analysis(dirname):
+	file_name = "result_" + os.path.basename(dirname[:len(dirname)-1]) + ".txt"
+	file = open(file_name, "w")
 	counter = collections.Counter()
+	i = 0
 
 	print("Analysis is started...")
 	
-	for filename in os.listdir(directory):
-		f = os.path.join(directory, filename)
+	for filename in os.listdir(dirname):
+		f = os.path.join(dirname, filename)
 		if os.path.isfile(f):
 			dic = pdf_analysis(f)
 			counter.update(dic)
-			print(".")
+			print("..." + str(i))
+			i+=1
 	res = dict(counter)
 	print("...analysis is finished")
+
 	print(res)
+	file.write(json.dumps(res))
+	file.close()
+	return file_name
+
+if __name__ == '__main__':
+
+	directories = ["../../pdf_data/clean_pdf_100_embed_video/", "../../pdf_data/malware_pdf_cve_sorted_173_files/", "../../pdf_data/clean_pdf_9000_files/", "../../pdf_data/MALWARE_PDF_PRE_04-2011_10982_files/"]
+
+	for dire in directories:
+		out = dir_analysis(dire)
+		print("Produced " + out)
