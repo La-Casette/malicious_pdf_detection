85b5304007b75bd566df044358014248ae60f490
MATTIA ROSSO s294711 <s294711@studenti.polito.it>
2022-11-27 18:12:37 +0100

    Fixed kmeans dim red

--- classification/data_preprocessing.py
+++ classification/data_preprocessing.py
@@ -15,7 +15,6 @@ class KMeansDimensionalityReduction():
         # Sum over the rows
         self.f_ben = np.sum(df_ben.to_numpy(), axis=0).reshape(-1,1) # f_ben[0] = # of benign pdf with that feature (at least one inside)
         self.f_mal = np.sum(df_mal.to_numpy(), axis=0).reshape(-1,1)
-        self.f_ben_mal = np.abs(self.f_ben - self.f_mal)
         # Apply kmeans to get most relevant features separately for benign and malign
         kmeans_ben = KMeans(n_clusters=2, random_state=0).fit(self.f_ben)
         highest_cluster_center = np.argmax(kmeans_ben.cluster_centers_)
@@ -25,13 +24,6 @@ class KMeansDimensionalityReduction():
         self.f_mal_rel = kmeans_mal.labels_ == highest_cluster_center
         self.rel_mask_df = np.hstack((self.f_ben_rel | self.f_mal_rel,[True]))
         self.rel_mask = self.f_ben_rel | self.f_mal_rel
-        # Apply kmeans to get most relevant features for |benign-malign|
-        kmeans_ben_mal = KMeans(n_clusters=2, random_state=0).fit(self.f_ben_mal)
-        highest_cluster_center = np.argmax(kmeans_ben_mal.cluster_centers_)
-        self.f_ben_mal_rel_diff = kmeans_ben_mal.labels_ == highest_cluster_center
-        self.rel_mask_df_diff = np.hstack((self.f_ben_mal_rel_diff,[True]))
-        self.rel_mask_diff = self.f_ben_mal_rel_diff
-        
 
     def fit_ben_mal_kmeans(self, df=None, X_tot=None, X_train=None):
         return df.iloc[:,self.rel_mask_df], X_tot[:,self.rel_mask], X_train[:,self.rel_mask]
@@ -39,8 +31,5 @@ class KMeansDimensionalityReduction():
     def transform_ben_mal_kmeans(self, X_test):
         return X_test[:,self.rel_mask]
 
-    def fit_diff_kmeans(self, df=None, X_tot=None, X_train=None):
-        return df.iloc[:,self.rel_mask_df_diff], X_tot[:,self.rel_mask_diff], X_train[:,self.rel_mask_diff]
-
-    def transform_diff_kmeans(self, X_test):
-        return X_test[:,self.f_ben_mal_rel_diff]
+    def get_kmeans_features(self):
+        return self.f_ben_rel, self.f_mal_rel
