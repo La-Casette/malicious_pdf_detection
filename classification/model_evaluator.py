32f72312d5ae0f487173fb0de532859d7cd2f73b
ippo <lorenzoippolito.99@gmail.com>
2022-12-01 23:24:00 +0100

    improving tree and random_forest

--- classification/model_evaluator.py
+++ classification/model_evaluator.py
@@ -15,40 +15,14 @@ def compute_scores(y_test, y_test_predicted):
     print('True positive: %d'%(knn_confmat[1, 1]))
     print('False negative: %d'%(knn_confmat[1, 0]))
     print('True negative: %d'%(knn_confmat[0, 0]))
-    print('False Negative Rate: %f%%'%((knn_confmat[1, 0]/(knn_confmat[1, 0] + knn_confmat[1, 1]))*100))
 
 def false_negative_loss_function(clf, X, y):
     y_pred = clf.predict(X)
     fn = metrics.confusion_matrix(y, y_pred)[1,0]
     f1 = metrics.f1_score(y, y_pred)
-    return {'false_neg':fn, 'f1_score':f1}
+    accuracy = metrics.accuracy_score(y, y_pred)
+    return {'false_neg':fn, 'f1_score':f1, 'accuracy_score':accuracy}
     
 def kfold_cross_validation(model, X, y, k=5):
     scores = cross_validate(model, X, y, cv=k, scoring=false_negative_loss_function)
-    #cv = KFold(n_splits=k, random_state=1, shuffle=True)
-    #if scoring == 'precision':
-    #    scores = cross_val_score(model, X, y.reshape(-1,), 
-    #                            scoring=metrics.make_scorer(metrics.precision_score, pos_label=0), # NPV metric
-    #                            cv=cv, 
-    #                            n_jobs=-1)
     return scores
-
-def compute_accuracy(y_test, y_test_predicted):
-    return metrics.accuracy_score(y_test, y_test_predicted)
-
-def compute_f1_score(y_test, y_test_predicted):
-    return metrics.f1_score(y_test, y_test_predicted)
-
-def compute_fpr(y_test, y_test_predicted):
-    return metrics.precision_score(y_test, y_test_predicted, pos_label=0)
-
-def get_train_test_split(df):
-    X_tot = df.iloc[:,:-1]
-    y_tot = df.iloc[:,-1:]
-    X_train, X_test, y_train, y_test= train_test_split(X_tot, y_tot,
-                                                        test_size= 0.2,
-                                                        shuffle= True, #shuffle the data to avoid bias
-                                                        stratify=df['malware'],
-                                                        random_state= 0)
-    return X_train, X_test, y_train, y_test
-
