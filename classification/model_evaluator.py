838bff980c16ad505f3cb7740e4ae0dd6b79e03d
ippo <lorenzoippolito.99@gmail.com>
2022-12-01 18:31:47 +0100

    implementation of preprocessing

--- classification/model_evaluator.py
+++ classification/model_evaluator.py
@@ -1,6 +1,6 @@
 import sklearn.metrics as metrics
 from sklearn.model_selection import KFold
-from sklearn.model_selection import cross_val_score
+from sklearn.model_selection import cross_validate
 import pandas as pd
 from sklearn.model_selection import train_test_split
 def compute_scores(y_test, y_test_predicted):
@@ -17,15 +17,20 @@ def compute_scores(y_test, y_test_predicted):
     print('True negative: %d'%(knn_confmat[0, 0]))
     print('False Negative Rate: %f%%'%((knn_confmat[1, 0]/(knn_confmat[1, 0] + knn_confmat[1, 1]))*100))
 
-def kfold_cross_validation(model, X, y, k=5, scoring='accuracy'):
-    cv = KFold(n_splits=k, random_state=1, shuffle=True)
-    if scoring == 'precision':
-        scores = cross_val_score(model, X, y.reshape(-1,), 
-                                scoring=metrics.make_scorer(metrics.precision_score, pos_label=0), # NPV metric
-                                cv=cv, 
-                                n_jobs=-1)
-    if scoring == 'accuracy':
-        scores = cross_val_score(model, X, y.reshape(-1,), scoring=scoring, cv=cv, n_jobs=-1)
+def false_negative_loss_function(clf, X, y):
+    y_pred = clf.predict(X)
+    fn = metrics.confusion_matrix(y, y_pred)[1,0]
+    f1 = metrics.f1_score(y, y_pred)
+    return {'false_neg':fn, 'f1_score':f1}
+    
+def kfold_cross_validation(model, X, y, k=5):
+    scores = cross_validate(model, X, y, cv=k, scoring=false_negative_loss_function)
+    #cv = KFold(n_splits=k, random_state=1, shuffle=True)
+    #if scoring == 'precision':
+    #    scores = cross_val_score(model, X, y.reshape(-1,), 
+    #                            scoring=metrics.make_scorer(metrics.precision_score, pos_label=0), # NPV metric
+    #                            cv=cv, 
+    #                            n_jobs=-1)
     return scores
 
 def compute_accuracy(y_test, y_test_predicted):
@@ -36,9 +41,6 @@ def compute_f1_score(y_test, y_test_predicted):
 
 def compute_fpr(y_test, y_test_predicted):
     return metrics.precision_score(y_test, y_test_predicted, pos_label=0)
-
-def get_false_positive(y_test, y_test_predicted):
-    return metrics.confusion_matrix(y_test, y_test_predicted)[0,1]
 
 def get_train_test_split(df):
     X_tot = df.iloc[:,:-1]
