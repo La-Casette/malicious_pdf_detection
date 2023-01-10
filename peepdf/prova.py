5947492f5f5bce5ea3c4f1be4779cb83e170b914
ippo <lorenzoippolito.99@gmail.com>
2023-01-10 14:08:11 +0100

    initial refactor

--- peepdf/prova.py
+++
@@ -1,8 +0,0 @@
-from PDFConsole import PDFConsole
-from PDFCore import PDFParser
-
-print("start")
-pdfParser = PDFParser()
-ret, pdf = pdfParser.parse("cv.pdf", True, False)
-print(pdf.getStats())
-print("Constains JS: " + pdf.containsJS())
